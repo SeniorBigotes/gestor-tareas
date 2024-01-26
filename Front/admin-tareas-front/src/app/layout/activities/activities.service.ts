c1ee1ef234bc7303a327a471eca32e72da3bf225
SeniorBigotes <mauriciopuente.pineda2000@gmail.com>
2024-01-25 22:11:02 -0600

    Acabando tarea seleccionada

--- Front/admin-tareas-front/src/app/layout/activities/activities.service.ts
+++ Front/admin-tareas-front/src/app/layout/activities/activities.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { ChangeDetectorRef, ElementRef, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivitiesService {
 
+  private showMore: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private showMoreElement: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
   constructor() { }
+  
+  get $showMore(): Observable<boolean> {
+    return this.showMore.asObservable();
+  }
+  get $showMoreElement(): Observable<boolean> {
+    return this.showMoreElement.asObservable();
+  }
+
+  checkOverflow(contentElement?: ElementRef) {    
+    if(contentElement) {
+      const element = contentElement.nativeElement
+      this.showMoreElement.next(element.scrollHeight > element.clientHeight);
+    }
+  }
+
+  toggleShowMore(showMore: boolean): void {
+    this.showMore.next(showMore);
+  }
 }
--- Front/admin-tareas-front/src/app/layout/activities/pages/activities/activities.component.ts
+++ Front/admin-tareas-front/src/app/layout/activities/pages/activities/activities.component.ts
@@ -1,25 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { Activities } from '../../../../models/IActivity';
-import { DataCarousel } from '../../../home/Interfaces';
 import { CarouselActivitiesComponent } from '../../components/carousel-activities/carousel-activities.component';
+import { AppService } from '../../../../app.service';
+import { CommonModule } from '@angular/common';
+import { ActivitiesService } from '../../activities.service';
 
 @Component({
   selector: 'app-activities',
   standalone: true,
-  imports: [HeaderComponent, CarouselActivitiesComponent],
+  imports: [CommonModule,
+    HeaderComponent, CarouselActivitiesComponent],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.scss'
 })
 export class ActivitiesComponent implements OnInit {
 
-  activities: Activities[] = []
-  dataCarousel?: DataCarousel;
-  itemsCarousel: Array<DataCarousel> = [];
+  /* función de "...más" */  
+  contentElement?: ElementRef;
+  // obtiene datos de la etiqueta seleccionada con plantilla #more
+  // ejecuta en setter, recibe la info de la etiqueta como parametro
+  @ViewChild('more', {static: false}) set content(content: ElementRef) {
+    if(content) {
+      this.contentElement = content;
+      this.activities.checkOverflow(this.contentElement);
+    }
+  };
+  showMore: boolean = false;
+  showMoreElement: boolean = false;
+  /* fin de función "...más" */
 
-  constructor() {}
+  task?: Activities;
+
+  constructor(private appService: AppService,
+              private activities: ActivitiesService,
+              private cdr: ChangeDetectorRef
+              ) {}
 
   ngOnInit(): void {
+    this.appService.getTasks().subscribe(task => this.task = task[0]);
 
+    this.activities.$showMore.subscribe(show => this.showMore = show);
+    this.activities.$showMoreElement.subscribe(show => {
+      this.showMoreElement = show;
+      this.cdr.detectChanges();
+    });
   } // end ngOnInit()
+
+  toggleReadMore() {
+    this.activities.toggleShowMore(!this.showMore);
+  }
 }
